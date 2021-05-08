9e941e2ec7031c9c903aa94031cb95363c23a893
George Gaskin <george_gaskin@msn.com>
2021-05-08 21:13:19 +0200

    Backup

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
+
 namespace interpolator
 {
     class Program
@@ -14,9 +15,14 @@ namespace interpolator
                         "#--productList:{{product}}\t{{price}}--#\n";
 
             var convTmplt = template.Interpolate(o => o
-                .Replace("product", () => "Gaming Product")
-                .Replace("price", () => "$200")
-                .Replace("shippingDate", () => DateTime.Today.ToShortDateString()));
+                .ForTemplate("productList",(t)=>{
+                    return t.Interpolate(t=>t
+                    .For("product","Keyboard")
+                    .For("price","$300"));
+                    })
+                .For("product", () => "Gaming Product")
+                .For("price", () => "$200")
+                .For("shippingDate", () => DateTime.Today.ToShortDateString()));
 
 
             Console.WriteLine(convTmplt);
@@ -34,97 +40,101 @@ namespace interpolator
         }
     }
 
-    public class LoopHelper {
-        public TemplateInterpolator For(string target, Func<string> action)
-        {
-            _directives.Add(target, action);
-            return this;
-        }
+    class DirectivesDict:Dictionary<string,Func<string,string>>
+    {
+        public DirectivesDict() {}
+        public DirectivesDict(DirectivesDict dict, StringComparer comparer ):base(dict,comparer) {}  
     }
 
-    public abstract class InterpolatorBase
+    public class TemplateInterpolator
     {
         private string _regexString = @"{{(\w+)}}";
         private bool _ignoreCase = true;
-        private readonly Dictionary<string, Func<string>> _directives =
-            new Dictionary<string, Func<string>>();
+        private string _tplteRegex = @"\#--(\w+):(.*)--\#";
+        private readonly DirectivesDict _directives = new DirectivesDict();
+    
+        public TemplateInterpolator For(string target, Func<string> action)
+        {
+            _directives.Add(target, (_)=>action.Invoke());
+            return this;
+        }
 
-        public InterpolatorBase SetDelimiters(string preDelimiter,string postDelimiter)
+    
+        public TemplateInterpolator For(string target, string value)
+        {
+            return For(target,()=>value);
+        }
+
+        public TemplateInterpolator ForTemplate(string templateName, Func<string,string> action)
+        {
+            _directives.Add(templateName, action);
+            return this;
+        }
+
+        public TemplateInterpolator SetDelimiters(string preDelimiter, string postDelimiter)
         {
             _regexString = $@"{preDelimiter}(\w+){postDelimiter}";
             return this;
         }
 
-        public InterpolatorBase IgnoreCase(bool ignoreCase)
+        public TemplateInterpolator IgnoreCase(bool ignoreCase)
         {
             _ignoreCase = ignoreCase;
             return this;
         }
+
+       
+
+
         public string Interpolate(string template)
+        {
+            var parsedTemplate = InterpolateTemplates(template);
+            parsedTemplate = InterpolateFieldNames(parsedTemplate);
+            return parsedTemplate;
+        }
+
+         private string InterpolateTemplates(string template)
+        {
+            var regExInstance = new Regex(_tplteRegex, RegexOptions.Compiled);
+            var result = regExInstance.Replace(template, m =>
+            {
+                var loopName = m.Groups[1].Value;
+                var template=m.Groups[2].Value;
+                // var found = _loopDirectives.TryGetValue(loopName, out var actions);
+                // if (found)
+                // {
+                //     return "";
+                // }
+                return "";
+            });
+
+            return result;
+        }
+
+        private string InterpolateFieldNames(string template)
         {
             var regExInstance = new Regex(_regexString, RegexOptions.Compiled);
             var directives = _ignoreCase ? ConvertDirectivesToCaseInsesitive(_directives) : _directives;
-            return regExInstance.Replace(template, match => GetNewValue(match, directives));
+            return regExInstance.Replace(template, match => GetNewValue(match, directives, template));
         }
 
-        private Dictionary<string, Func<string>> ConvertDirectivesToCaseInsesitive(Dictionary<string, Func<string>> directives)
+        private DirectivesDict ConvertDirectivesToCaseInsesitive(DirectivesDict directives)
         {
             var comparer = StringComparer.OrdinalIgnoreCase;
-            return new Dictionary<string, Func<string>>(directives, comparer);
+            return new DirectivesDict(directives, comparer);
         }
 
-        private string GetNewValue(Match match, Dictionary<string, Func<string>> directives)
+        private string GetNewValue(Match match, DirectivesDict directives, string template)
         {
             var fieldName = match.Groups[1].Value;
             var found = directives.TryGetValue(fieldName, out var action);
             if (found)
             {
-                return action();
+                return action(template);
             }
             var originalValue = match.Groups[0].Value;
             return originalValue;
         }
-    }
 
-    public class TemplateInterpolator:InterpolatorBase
-    {
-       
-        public TemplateInterpolator For(string target, Func<string> action)
-        {
-            _directives.Add(target, action);
-            return this;
-        }
-
-        public TemplateInterpolator ForLoop(string target, Action<LoopHelper> actions)
-        {
-            _directives.Add(target, action);
-            return this;
-        }
-       
-
-        // public string Interpolate(string template)
-        // {
-        //     var regExInstance = new Regex(_regexString, RegexOptions.Compiled);
-        //     var directives = _ignoreCase ? ConvertDirectivesToCaseInsesitive(_directives) : _directives;
-        //     return regExInstance.Replace(template, match => GetNewValue(match, directives));
-        // }
-
-        // private Dictionary<string, Func<string>> ConvertDirectivesToCaseInsesitive(Dictionary<string, Func<string>> directives)
-        // {
-        //     var comparer = StringComparer.OrdinalIgnoreCase;
-        //     return new Dictionary<string, Func<string>>(directives, comparer);
-        // }
-
-        // private string GetNewValue(Match match, Dictionary<string, Func<string>> directives)
-        // {
-        //     var fieldName = match.Groups[1].Value;
-        //     var found = directives.TryGetValue(fieldName, out var action);
-        //     if (found)
-        //     {
-        //         return action();
-        //     }
-        //     var originalValue = match.Groups[0].Value;
-        //     return originalValue;
-        // }
     }
 }
